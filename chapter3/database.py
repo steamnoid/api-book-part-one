e275470f4533bdcdd8a417b11bdcb3476689ce8d
krzysztofpaliga <picospitfire@icloud.com>
2026-01-02 12:58:00 +0100

    Adds comprehensive crusd db tests

--- chapter3/database.py
+++ chapter3/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ormfrom declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./fantasy_data.db"
